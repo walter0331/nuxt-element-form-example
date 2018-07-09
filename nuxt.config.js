148f8b805c1c2c5e587ebcbb9eadbc76377576fc
walter0331 <walter1584@gmail.com>
2018-07-09 12:41:20 +0800

    rename

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   */
   mode: 'spa',
   head: {
-    title: 'nuxt-lol-form',
+    title: 'nuxt-element-form-example',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
